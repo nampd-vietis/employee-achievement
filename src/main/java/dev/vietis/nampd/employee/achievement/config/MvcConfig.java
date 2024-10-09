820c7f1619026a905aee50f1eb1934dbd8abb9ed
nampd-vietis <nampd@vietis.com.vn>
2024-10-09 16:01:45 +0700

    add language english

--- src/main/java/dev/vietis/nampd/employee/achievement/config/MvcConfig.java
+++ src/main/java/dev/vietis/nampd/employee/achievement/config/MvcConfig.java
@@ -2,9 +2,17 @@ package dev.vietis.nampd.employee.achievement.config;
 
 import dev.vietis.nampd.employee.achievement.auth.AuthInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -20,6 +28,32 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/admin/**", "/user/**")
                 .excludePathPatterns("/login", "/logout");
+
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        Locale vietnamLocale = new Locale("vi");
+        slr.setDefaultLocale(vietnamLocale);
+        return slr;
+    }
+
+    @Bean("messageSource")
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasenames("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
 
